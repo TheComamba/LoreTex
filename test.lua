cb0ef4f6cbe22ef10602b1e297c47c3748750133
TheComamba <thecomamba@jojoheinze.de>
2022-09-03 10:16:58 +0200

    Tests for common functionalities.

--- test.lua
+++ test.lua
@@ -1,13 +1,52 @@
+local testFiles = { "common" }
+local numSucceeded = 0
+local numFailed = 0
+
+local function resetEnvironment()
+    Entities = {}
+    Histories = {}
+end
+
+function Assert(caller, expected, out)
+    if expected == out then
+        numSucceeded = numSucceeded + 1
+    else
+        numFailed = numFailed + 1
+        tex.print([[Error in function "]] .. caller .. [["!\\]])
+        if type(expected) ~= type(out) then
+            tex.print("Expected output of type ")
+            tex.print(type(expected) .. ",")
+            tex.print("but received output of type ")
+            tex.print(type(out) .. [[.\\]])
+        else
+            tex.print("Expected: ")
+            tex.print(TexCmd("begin", "verbatim"))
+            tex.print(expected)
+            tex.print(TexCmd("end", "verbatim"))
+            tex.print("Received:")
+            tex.print(TexCmd("begin", "verbatim"))
+            tex.print(out)
+            tex.print(TexCmd("end", "verbatim"))
+        end
+    end
+end
+
 function RunTests()
     local out = {}
-    local numSucceeded = 0
-    local numFaield = 0
+
+    --TODO: Loop through all files in  folder.
+    for key, testfile in pairs(testFiles) do
+        resetEnvironment()
+        dofile(RelativePath .. "/tests/" .. testfile .. ".lua")
+    end
+
+    Append(out, TexCmd("section*", "Results"))
     Append(out, TexCmd("textsc", "Rpg"))
     Append(out, TexCmd("TeX"))
     Append(out, " ran ")
-    Append(out, numSucceeded + numFaield)
-    Append(out, " test, ")
-    Append(out, numFaield)
+    Append(out, numSucceeded + numFailed)
+    Append(out, " tests, ")
+    Append(out, numFailed)
     Append(out, " of which failed.")
     tex.print(table.concat(out))
 end
---
+++ tests/common.lua
@@ -0,0 +1,29 @@
+local list = { "a", "b", "c" }
+local expected = [[\begin{itemize}\footnotesize{}\item{} a\item{} b\item{} c\end{itemize}]]
+Assert("ListAll", expected, ListAll(list))
+
+local FirstNonWhitespaceCharTestArgs = {
+    { [[]], nil },
+    { [[     ]], nil },
+    { [[ABC]], 1 },
+    { [[ABC   ]], 1 },
+    { [[ AB C]], 2 },
+    { [[ Ä]], 2 },
+    { [[ \]], 2 },
+    { "\t \nA", 4 },
+    { [[
+
+    A
+    ]], 6 }
+}
+for key, inAndOut in pairs(FirstNonWhitespaceCharTestArgs) do
+    Assert("FirstNonWhitespaceChar", inAndOut[2], FirstNonWhitespaceChar(inAndOut[1]))
+end
+
+local ReplaceTestArgs = {
+    { [[Har]], [[De]], [[Harmonic minor]], [[Demonic minor]] },
+    { [[\myref]], [[\ref]], [[Well, \myref{cake} is here.]], [[Well, \ref{cake} is here.]] }
+}
+for key, args in pairs(ReplaceTestArgs) do
+    Assert("Replace", args[4], Replace(args[1], args[2], args[3]))
+end

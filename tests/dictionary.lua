648bbf488d8f4dba1fb0985fd9be5c737ec26999
TheComamba <thecomamba@jojoheinze.de>
2022-09-29 21:30:29 +0200

    More tests for tables.

--- tests/dictionary.lua
+++ tests/dictionary.lua
@@ -17,3 +17,52 @@ local expected = { [[\tablehead{\clmhead{a}&\clmhead{b}\\\hline}]],
 }
 
 Assert("PrintTable", expected, PrintTable(tab))
+
+local tab = { { "a", "b", "c" },
+    { "d", "e", "f" } }
+
+local expected = { [[\tablehead{\clmhead{a}&\clmhead{b}&\clmhead{c}\\\hline}]],
+    [[\begin{center}]],
+    [[\begin{supertabular}]],
+    [[{]],
+    [[p{0.142\textwidth}]],
+    [[p{0.142\textwidth}]],
+    [[p{0.142\textwidth}]],
+    [[}]],
+    [[d]],
+    [[&]],
+    [[e]],
+    [[&]],
+    [[f]],
+    [[\\]],
+    [[\end{supertabular}]],
+    [[\end{center}]]
+}
+
+Assert("PrintTable", expected, PrintTable(tab))
+
+local tab = { { "a", "b", "c", "d" },
+    { "e", "f", "g", "h" } }
+
+local expected = { [[\tablehead{\clmhead{a}&\clmhead{b}&\clmhead{c}&\clmhead{d}\\\hline}]],
+    [[\begin{center}]],
+    [[\begin{supertabular}]],
+    [[{]],
+    [[p{0.101\textwidth}]],
+    [[p{0.101\textwidth}]],
+    [[p{0.101\textwidth}]],
+    [[p{0.101\textwidth}]],
+    [[}]],
+    [[e]],
+    [[&]],
+    [[f]],
+    [[&]],
+    [[g]],
+    [[&]],
+    [[h]],
+    [[\\]],
+    [[\end{supertabular}]],
+    [[\end{center}]]
+}
+
+Assert("PrintTable", expected, PrintTable(tab))

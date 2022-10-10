32d8a2279289035283e1e073265c7d2796731e85
Lastor <simon@jojoheinze.de>
2022-10-10 16:25:20 +0200

    Better test for entities with history.

--- tests/entities-with-history.lua
+++ tests/entities-with-history.lua
@@ -4,12 +4,39 @@ NewEntity("test-2", "place", nil, "Test 2")
 AddEvent(CurrentEntity(), -10, [[Event that concerns \reference{test-1} and \itref{test-2}.]])
 AddEvent(CurrentEntity(), 10, [[Event in the future.]])
 
-AddAllEntitiesToPrimaryRefs()
+AddRef("test-1", PrimaryRefs)
 
 IsShowFuture = false
+
 local out = AutomatedChapters()
 
 local expected = {
+    [[\chapter{Orte}]],
+    [[\section*{Alle Orte}]],
+    [[\begin{itemize}]],
+    [[\item{} \nameref{test-1}]],
+    [[\end{itemize}]],
+    [[\section{Orte}]],
+    [[\subsection{In der ganzen Welt}]],
+    [[\subsubsection{Test 1}]],
+    [[\label{test-1}]],
+    [[\paragraph{Histori\"e}]],
+    [[\begin{itemize}]],
+    [[\item{} -10 Vin (vor 10 Jahren): Event that concerns \nameref{test-1} and \itref{test-2}.]],
+    [[\end{itemize}]],
+    [[\chapter{Nur erwähnt}]],
+    [[\subparagraph{Test 2}]],
+    [[\label{test-2}]],
+    [[\hspace{1cm}]]
+}
+
+Assert("one-entity-with-history", expected, out)
+
+AddRef("test-2", PrimaryRefs)
+
+out = AutomatedChapters()
+
+expected = {
     [[\chapter{Orte}]],
     [[\section*{Alle Orte}]],
     [[\begin{itemize}]],
@@ -32,4 +59,4 @@ local expected = {
     [[\end{itemize}]]
 }
 
-Assert("entities-with-history", expected, out)
+Assert("two-entities-with-history", expected, out)

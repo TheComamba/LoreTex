61ffb1f91a4da700df2b3161439ae6c4a60380d4
TheComamba <thecomamba@jojoheinze.de>
2022-10-14 20:26:04 +0200

    Fixing association tests.

--- tests/entities-with-associations.lua
+++ tests/entities-with-associations.lua
@@ -56,4 +56,46 @@ SetDescriptor(place2, "location", "place-4")
 
 out = AutomatedChapters()
 
+local expected = {
+    [[\chapter{Orte}]],
+    [[\section*{Alle Orte}]],
+    [[\begin{itemize}]],
+    [[\item{} \nameref{place-1}]],
+    [[\item{} \nameref{place-2}]],
+    [[\end{itemize}]],
+    [[\section{Orte}]],
+    [[\subsection{In der ganzen Welt}]],
+    [[\subsubsection{Place 1}]],
+    [[\label{place-1}]],
+    [[\paragraph{Zusammenschlüsse}]],
+    [[\begin{itemize}]],
+    [[\item{} Mitglied der \nameref{orga}.]],
+    [[\end{itemize}]],
+    [[\subsection{In Place 4}]],
+    [[\subsubsection{Place 2}]],
+    [[\label{place-2}]],
+    [[\paragraph{Zusammenschlüsse}]],
+    [[\begin{itemize}]],
+    [[\item{} Hometown der \nameref{orga}.]],
+    [[\end{itemize}]],
+    [[\chapter{Zusammenschlüsse}]],
+    [[\section*{Alle Zusammenschlüsse}]],
+    [[\begin{itemize}]],
+    [[\item{} \nameref{orga}]],
+    [[\end{itemize}]],
+    [[\section{Organisationen}]],
+    [[\subsection{In Place 3}]],
+    [[\subsubsection{Orga}]],
+    [[\label{orga}]],
+    [[\paragraph{Orte}]],
+    [[\begin{itemize}]],
+    [[\item{} \nameref{place-1}]],
+    [[\item{} \nameref{place-2} (Hometown, in \nameref{place-4})]],
+    [[\end{itemize}]],
+    [[\chapter{Nur erwähnt}]],
+    [[\subparagraph{Place 4}]],
+    [[\label{place-4}]],
+    [[\hspace{1cm}]]
+}
+
 Assert("entities-with-associations-and-locations", expected, out)

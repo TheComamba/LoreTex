d7800d2004e48dc625cd432d5abfbae93e7f2c22
Simon Heinze <simon@jojoheinze.de>
2023-01-14 17:50:39 +0100

    Debug command to print descriptors immediately.

--- src/debug.lua
+++ src/debug.lua
@@ -53,3 +53,13 @@ local function printAllTypes()
 end
 
 Debug.printAllTypes = printAllTypes
+
+Debug.printDescriptorsImmediately = function()
+    local setDescriptorOriginal = TexApi.setDescriptor
+    ---@diagnostic disable-next-line: duplicate-set-field
+    TexApi.setDescriptor = function(arg)
+        tex.print(arg.description)
+        tex.print(arg.descriptor)
+        setDescriptorOriginal(arg)
+    end
+end

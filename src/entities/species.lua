5373572f7df8114b41ad69c752ba2ace059a03a3
Simon Heinze <simon@jojoheinze.de>
2023-01-15 10:47:00 +0100

    Only print lifestages if the species has some aging defined.

--- src/entities/species.lua
+++ src/entities/species.lua
@@ -142,7 +142,7 @@ function AddLifestageHistoryItems(entity)
 		return
 	end
 	local species = GetProtectedNullableField(entity, "species", false)
-	if species == nil then
+	if species == nil or not hasDefinedAging(species) then
 		return
 	end
 	local deathyear = GetProtectedNullableField(entity, "died")

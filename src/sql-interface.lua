6e3dd013bd467fba75cd2ff77988c8ed8308bd9b
Simon Heidrich <simon@jojoheinze.de>
2023-04-02 18:46:11 +0200

    Stopped loading luasql module.

--- src/sql-interface.lua
+++ src/sql-interface.lua
@@ -1,11 +1,6 @@
 
 local function writeEntityToDatabase(entity)
-    local luasql = require "luasql.sqlite3"
-    local sqlite = luasql.sqlite3()
-    if sqlite == nil then
-        LogError("Sqlite module could not be loaded.")
-        return
-    end
+    LogError("writeEntityToDatabase is not yet implemented.")
 end
 
 TexApi.writeLoreToDatabase = function(path)

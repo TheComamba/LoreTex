e9e8488ac492acb1816ddb8e7ee57b2a0b716d55
Simon Heidrich <simon@jojoheinze.de>
2023-04-02 18:04:41 +0200

    Trying to load luasql module.

--- src/sql-interface.lua
+++ src/sql-interface.lua
@@ -1,3 +1,15 @@
-TexApi.writeLoreToDatabase = function (path)
-    LogError("writeLoreToDatabase is not yet implemented.")
+
+local function writeEntityToDatabase(entity)
+    local luasql = require "luasql.sqlite3"
+    local sqlite = luasql.sqlite3()
+    if sqlite == nil then
+        LogError("Sqlite module could not be loaded.")
+        return
+    end
+end
+
+TexApi.writeLoreToDatabase = function(path)
+    for key, entity in pairs(AllEntities) do
+        writeEntityToDatabase(entity)
+    end
 end

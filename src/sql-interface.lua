4de1c02e1a1540f664d5672fd752df43edebf5f5
Simon Heinze <simon@jojoheinze.de>
2023-04-03 21:43:28 +0200

    Trying to begin convesation between lua and rust.

--- src/sql-interface.lua
+++ src/sql-interface.lua
@@ -1,5 +1,23 @@
 
 local function writeEntityToDatabase(entity)
+    local ffi = require("ffi")
+    if ffi == nil then
+        LogError("Cannot load ffi module.")
+        return
+    end
+
+    local rust_lib = ffi.load("./rust/target/debug/liblore_tex.so")
+    if rust_lib == nil then
+        LogError("Cannot load rust library.")
+        return
+    end
+
+    ffi.cdef[[
+        int test();
+    ]]
+
+    testresult = rust_lib.test()
+    tex.print(testresult)
     LogError("writeEntityToDatabase is not yet implemented.")
 end
 

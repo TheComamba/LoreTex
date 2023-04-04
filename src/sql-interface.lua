56fd3275701dad503fe59add30aa8710e50676d2
Simon Heinze <simon@jojoheinze.de>
2023-04-04 18:15:50 +0200

    Error message when running in restricted mode.

--- src/sql-interface.lua
+++ src/sql-interface.lua
@@ -6,6 +6,15 @@ local function writeEntityToDatabase(entity)
         return
     end
 
+    if ffi["load"] == nil then
+        LogError[[
+LuaLaTex has been called in restricted mode, which does not allow the loading of external libraries.
+
+If you're running from a terminal, call the \verb'lualatex' command with the \verb'--enable-write18' option.
+]]
+        return
+    end
+
     local rust_lib = ffi.load("./rust/target/debug/liblore_tex.so")
     if rust_lib == nil then
         LogError("Cannot load rust library.")

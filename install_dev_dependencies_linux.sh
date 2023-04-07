2b63b5227adb6cb818d56051c5bf087ae6aed004
Simon Heinze <simon@jojoheinze.de>
2023-04-07 06:56:01 +0200

    Adding diesel and cbindgen to dev dependency installation script.

--- install_dev_dependencies_linux.sh
+++ install_dev_dependencies_linux.sh
@@ -11,3 +11,6 @@ rust_diesel_req=libsqlite3-dev
 rust_rfd_req=libgtk-3-dev
 
 sudo apt install $rust_diesel_req $rust_rfd_req
+
+cargo install diesel_cli --no-default-features --features sqlite
+cargo install cbindgen

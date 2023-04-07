e7c5215f9df9f935fd60254a47dea783bc86db7b
Simon Heinze <simon@jojoheinze.de>
2023-04-07 07:26:04 +0200

    First attempt at reading header into lua.

--- src/sql-interface.lua
+++ src/sql-interface.lua
@@ -1,9 +1,8 @@
-
-local function writeEntityToDatabase(entity)
+local function getFFIModule()
     local ffi = require("ffi")
     if ffi == nil then
         LogError("Cannot load ffi module.")
-        return
+        return nil
     end
 
     if ffi["load"] == nil then
@@ -12,23 +11,51 @@ LuaLaTex has been called in restricted mode, which does not allow the loading of
 
 If you're running from a terminal, call the \verb'lualatex' command with the \verb'--enable-write18' option.
 ]]
-        return
+        return nil
     end
+
+    return ffi
+end
+
+local function getCHeader()
+    local headerPath = RelativePath .. [[../rust/loretex_api.h]]
+    local file = io.open(headerPath, "r")
+    if not file then
+        LogError("Cann load header file.")
+        return nil
+    end
+    local content = file:read "*all"
+    file:close()
+    return content
+end
+
+local function getLib()
+    local ffi = getFFIModule()
+    if not ffi then return nil end
+
+    local header = getCHeader()
+    if not header then return nil end
+    ffi.cdef(header)
 
     local libPath = RelativePath .. [[../rust/target/debug/libloretex.so]]
     local rustLib = ffi.load(libPath)
-    if rustLib == nil then
+    if not rustLib then
         LogError("Cannot load rust library.")
-        return
+        return nil
     end
 
-    ffi.cdef[[
-        int test();
-    ]]
+    return rustLib
+end
 
-    testresult = rustLib.test()
-    tex.print(testresult)
-    LogError("writeEntityToDatabase is not yet implemented.")
+
+local function writeEntityToDatabase(entity)
+    local rustLib = getLib()
+    if not rustLib then return nil end
+    result = rustLib.write_database_column()
+    if result != 0 then
+        LogError("Something went wrong during writeEntityToDatabase. No idea what, though.")
+        return
+    end
 end
 
 TexApi.writeLoreToDatabase = function(path)

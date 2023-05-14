067b6146c20f7112e017cdf1a7bd5b0299cce78d
TheComamba <thecomamba@jojoheinze.de>
2023-05-14 14:12:46 +0200

    Updated workflow.

--- install_build_dependencies_linux.sh
+++ install_build_dependencies_linux.sh
@@ -4,7 +4,7 @@ set -e
 if ! command -v lualatex &> /dev/null
 then
     echo "Installing lualatex"
-    sudo apt-get install lualatex
+    sudo apt-get install texlive-lualatex
 fi
 
 url=$(<required_lorecore_release.txt)

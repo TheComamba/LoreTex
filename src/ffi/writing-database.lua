d91272a86a04cbae408e71f287f7c5b6b06c3bfd
TheComamba <thecomamba@jojoheinze.de>
2023-06-11 23:21:44 +0200

    Refactoring: Splitting files.

--- src/ffi/writing-database.lua
+++ src/ffi/writing-database.lua
@@ -1,60 +1,3 @@
-local ffi
-local loreCore
-
-local function getFFIModule()
-    if ffi then return ffi end
-
-    ffi = require("ffi")
-    if ffi == nil then
-        LogError("Cannot load ffi module.")
-        return nil
-    end
-
-    if ffi["load"] == nil then
-        LogError [[
-LuaLaTex cannot access the ffi (foreign function interface) library.
-This is most likely because it has been called in restricted mode, which does not allow the loading of external libraries.
-You need to call it with the \verb'--shell-escape' option.
-See the installation section of README.md on how to do that.
-]]
-        return nil
-    end
-
-    return ffi
-end
-
-local function getCHeader()
-    local headerPath = RelativePath .. [[../dependencies/lorecore_api.h]]
-    local file = io.open(headerPath, "r")
-    if not file then
-        LogError("Cannot load header file at " .. headerPath .. ".")
-        return nil
-    end
-    local content = file:read "*all"
-    file:close()
-    return content
-end
-
-local function getLib()
-    if loreCore then return loreCore end
-
-    ffi = getFFIModule()
-    if not ffi then return nil end
-
-    local header = getCHeader()
-    if not header then return nil end
-    ffi.cdef(header)
-
-    local libPath = RelativePath .. [[../dependencies/liblorecore.so]]
-    loreCore = ffi.load(libPath)
-    if not loreCore then
-        LogError("Cannot load rust library.")
-        return nil
-    end
-
-    return loreCore
-end
-
 local function optionalEntityToString(inp)
     if IsEntity(inp) then
         local label = GetProtectedStringField(inp, "label")
@@ -77,7 +20,7 @@ local function shouldDescriptorBeWrittenToDatabase(descriptor)
 end
 
 local function createEntityColumn(label, descriptor, description)
-    ffi = getFFIModule()
+    local ffi = getFFIModule()
     if not ffi then return nil end
 
     local column = {};
@@ -116,7 +59,7 @@ local function getEntityColumns()
 end
 
 local function getCEntityColumns()
-    ffi = getFFIModule()
+    local ffi = getFFIModule()
     if not ffi then return nil end
 
     local entityColumns = getEntityColumns()
@@ -128,8 +71,8 @@ local function getCEntityColumns()
 end
 
 local function writeEntitiesToDatabase(dbPath)
-    ffi = getFFIModule()
-    loreCore = getLib()
+    local ffi = getFFIModule()
+    local loreCore = getLib()
     if not loreCore or not ffi then return nil end
 
     local cEntityColumns, count = getCEntityColumns()
@@ -142,7 +85,7 @@ local function writeEntitiesToDatabase(dbPath)
 end
 
 local function getCHistoryItemsList()
-    ffi = getFFIModule()
+    local ffi = getFFIModule()
     if not ffi then return nil end
 
     local historyItems = ffi.new("CHistoryItem[" .. #AllHistoryItems .. "]")
@@ -163,8 +106,8 @@ local function getCHistoryItemsList()
 end
 
 local function writeHistoryItemsToDatabase(dbPath)
-    ffi = getFFIModule()
-    loreCore = getLib()
+    local ffi = getFFIModule()
+    local loreCore = getLib()
     if not loreCore or not ffi then return nil end
 
     local items, count = getCHistoryItemsList()
@@ -196,7 +139,7 @@ local function getRelationships()
 end
 
 local function getCRelationships()
-    ffi = getFFIModule()
+    local ffi = getFFIModule()
     if not ffi then return nil end
 
     local relationships = getRelationships()
@@ -208,8 +151,8 @@ local function getCRelationships()
 end
 
 local function writeRelationshiptsToDatabase(dbPath)
-    ffi = getFFIModule()
-    loreCore = getLib()
+    local ffi = getFFIModule()
+    local loreCore = getLib()
     if not loreCore or not ffi then return nil end
 
     local cRelationships, count = getCRelationships()

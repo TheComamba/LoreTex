16bb68025a1492714c175af435459336dd32b48f
TheComamba <thecomamba@jojoheinze.de>
2023-07-12 00:07:08 +0200

    Adjusting test for aging subentities.

--- tests/testfiles/aging-subentities.lua
+++ tests/testfiles/aging-subentities.lua
@@ -1,13 +1,18 @@
-TexApi.newEntity { label = "char", type = "NPCs", name = "char" }
-TexApi.born { year = -15, event = "Born." }
-TexApi.setSpecies("species")
-TexApi.setDescriptor { descriptor = "subchar", description = [[\paragraph{subsubchar}\label{subsubchar}
-\paragraph{subsubsubchar}\label{subsubsubchar}]] }
+local function entitySetup(level)
+    TexApi.newEntity { label = "species-1", type = "species", name = "species-1" }
+    TexApi.setDescriptor { descriptor = "species-2", description = [[\label{species-2}
+\paragraph{species-3}\label{species-3}
+\subparagraph{species-4}\label{species-4}]] }
+    TexApi.setAgeFactor(1)
 
-TexApi.newEntity { label = "species", type = "species", name = "species" }
-TexApi.setDescriptor { descriptor = "subspecies", description = [[\paragraph{subsubspecies}\label{subsubspecies}
-\subparagraph{subsubsubspecies}\label{subsubsubspecies}]] }
-TexApi.setAgeFactor(1)
+    TexApi.newEntity { label = "char-1", type = "NPCs", name = "char-1" }
+    TexApi.born { year = -15, event = "Born." }
+    TexApi.setDescriptor { descriptor = "char-2", description = [[\label{char-2}
+\paragraph{char-3}\label{char-3}
+\subparagraph{char-4}\label{char-4}]] }
+
+    TexApi.setSpecies("species-" .. level)
+end
 
 local function setup()
     TexApi.makeAllEntitiesPrimary()
@@ -16,54 +21,76 @@ local function setup()
     TexApi.addType { metatype = "peoples", type = "species" }
 end
 
-local expected = {}
-Append(expected, [[\chapter{NPCs}]])
-Append(expected, [[\section*{]] .. CapFirst(Tr("all")) .. [[ NPCs}]])
-Append(expected, [[\begin{itemize}]])
-Append(expected, [[\item \nameref{char}]])
-Append(expected, [[\item \nameref{subsubchar}]])
-Append(expected, [[\end{itemize}]])
-Append(expected, [[\section{]] .. CapFirst(Tr("in_whole_world")) .. [[}]])
-Append(expected, [[\subsection{char}]])
-Append(expected, [[\label{char}]])
-Append(expected, [[\subsubsection{]] .. CapFirst(Tr("appearance")) .. [[}]])
-Append(expected, [[\paragraph{]] .. CapFirst(Tr("species_and_age")) .. [[:}]])
-Append(expected, [[\nameref{species}, 15 ]] .. Tr("years_old") .. [[.]])
-Append(expected, [[\subsubsection{]] .. CapFirst(Tr("history")) .. [[}]])
-Append(expected, [[\begin{itemize}]])
-Append(expected, [[\item -15 (]] .. Tr("x_years_ago", { 15 }) .. [[):\\Born.]])
-Append(expected,
-    [[\item -3 (]] .. Tr("x_years_ago", { 3 }) .. [[):\\ \nameref{char} ]] .. Tr("is") .. [[ ]] .. Tr("juvenile") ..
-    [[.]])
-Append(expected, [[\end{itemize}]])
-Append(expected, [[\subsubsection{Subchar}]])
-Append(expected, [[\paragraph{Subsubchar}]])
-Append(expected, [[\label{subsubchar}]])
+local function generateExpected(level)
+    local expected = {}
+    Append(expected, [[\chapter{NPCs}]])
+    Append(expected, [[\section*{]] .. CapFirst(Tr("all")) .. [[ NPCs}]])
+    Append(expected, [[\begin{itemize}]])
+    Append(expected, [[\item \nameref{char-1}]])
+    Append(expected, [[\item \nameref{char-2}]])
+    Append(expected, [[\item \nameref{char-3}]])
+    Append(expected, [[\item \nameref{char-4}]])
+    Append(expected, [[\end{itemize}]])
+    Append(expected, [[\section{]] .. CapFirst(Tr("in_whole_world")) .. [[}]])
+    Append(expected, [[\subsection{Char-1}]])
+    Append(expected, [[\label{char-1}]])
 
-Append(expected, [[\chapter{Species}]])
-Append(expected, [[\section*{]] .. CapFirst(Tr("all")) .. [[ Species}]])
-Append(expected, [[\begin{itemize}]])
-Append(expected, [[\item \nameref{species}]])
-Append(expected, [[\item \nameref{subsubspecies}]])
-Append(expected, [[\end{itemize}]])
-Append(expected, [[\section{]] .. CapFirst(Tr("in_whole_world")) .. [[}]])
-Append(expected, [[\subsection{species}]])
-Append(expected, [[\label{species}]])
-Append(expected, [[\subsubsection{]] .. CapFirst(Tr("lifestages")) .. [[}]])
-Append(expected, [[\paragraph{\LoreTexSort{1}]] .. CapFirst(Tr("child")) .. [[}]])
-Append(expected, [[0-12 ]] .. Tr("years"))
-Append(expected, [[\paragraph{\LoreTexSort{2}]] .. CapFirst(Tr("juvenile")) .. [[}]])
-Append(expected, [[12-20 ]] .. Tr("years"))
-Append(expected, [[\paragraph{\LoreTexSort{3}]] .. CapFirst(Tr("young")) .. [[}]])
-Append(expected, [[20-30 ]] .. Tr("years"))
-Append(expected, [[\paragraph{\LoreTexSort{4}]] .. CapFirst(Tr("adult")) .. [[}]])
-Append(expected, [[30-60 ]] .. Tr("years"))
-Append(expected, [[\paragraph{\LoreTexSort{5}]] .. CapFirst(Tr("old")) .. [[}]])
-Append(expected, [[60-90 ]] .. Tr("years"))
-Append(expected, [[\paragraph{\LoreTexSort{6}]] .. CapFirst(Tr("ancient")) .. [[}]])
-Append(expected, [[90+ ]] .. Tr("years"))
-Append(expected, [[\subsubsection{Subspecies}]])
-Append(expected, [[\paragraph{Subsubspecies}]])
-Append(expected, [[\label{subsubspecies}]])
+    Append(expected, [[\subsubsection{]] .. CapFirst(Tr("appearance")) .. [[}]])
+    Append(expected, [[\paragraph{]] .. CapFirst(Tr("species_and_age")) .. [[:}]])
+    Append(expected, [[\nameref{species-]] .. level .. [[}, 15 ]] .. Tr("years_old") .. [[.]])
 
-AssertAutomatedChapters("Entity with subentities and age", expected, setup)
+    Append(expected, [[\subsubsection{Char-2}]])
+    Append(expected, [[\label{char-2}]])
+    Append(expected, [[\paragraph{Char-3}]])
+    Append(expected, [[\label{char-3}]])
+    Append(expected, [[\subparagraph{Char-4}]])
+    Append(expected, [[\label{char-4}]])
+
+    Append(expected, [[\subsubsection{]] .. CapFirst(Tr("history")) .. [[}]])
+    Append(expected, [[\begin{itemize}]])
+    Append(expected, [[\item -15 (]] .. Tr("x_years_ago", { 15 }) .. [[):\\Born.]])
+    Append(expected,
+        [[\item -3 (]] .. Tr("x_years_ago", { 3 }) .. [[):\\ \nameref{char} ]] .. Tr("is") .. [[ ]] .. Tr("juvenile") ..
+        [[.]])
+    Append(expected, [[\end{itemize}]])
+
+    Append(expected, [[\chapter{Species}]])
+    Append(expected, [[\section*{]] .. CapFirst(Tr("all")) .. [[ Species}]])
+    Append(expected, [[\begin{itemize}]])
+    Append(expected, [[\item \nameref{species-1}]])
+    Append(expected, [[\item \nameref{species-2}]])
+    Append(expected, [[\item \nameref{species-3}]])
+    Append(expected, [[\item \nameref{species-4}]])
+    Append(expected, [[\end{itemize}]])
+    Append(expected, [[\section{]] .. CapFirst(Tr("in_whole_world")) .. [[}]])
+    Append(expected, [[\subsection{Species-1}]])
+    Append(expected, [[\label{species-1}]])
+
+    Append(expected, [[\subsubsection{]] .. CapFirst(Tr("lifestages")) .. [[}]])
+    Append(expected, [[\paragraph{\LoreTexSort{1}]] .. CapFirst(Tr("child")) .. [[}]])
+    Append(expected, [[0-12 ]] .. Tr("years"))
+    Append(expected, [[\paragraph{\LoreTexSort{2}]] .. CapFirst(Tr("juvenile")) .. [[}]])
+    Append(expected, [[12-20 ]] .. Tr("years"))
+    Append(expected, [[\paragraph{\LoreTexSort{3}]] .. CapFirst(Tr("young")) .. [[}]])
+    Append(expected, [[20-30 ]] .. Tr("years"))
+    Append(expected, [[\paragraph{\LoreTexSort{4}]] .. CapFirst(Tr("adult")) .. [[}]])
+    Append(expected, [[30-60 ]] .. Tr("years"))
+    Append(expected, [[\paragraph{\LoreTexSort{5}]] .. CapFirst(Tr("old")) .. [[}]])
+    Append(expected, [[60-90 ]] .. Tr("years"))
+    Append(expected, [[\paragraph{\LoreTexSort{6}]] .. CapFirst(Tr("ancient")) .. [[}]])
+    Append(expected, [[90+ ]] .. Tr("years"))
+
+    Append(expected, [[\subsubsection{Species-2}]])
+    Append(expected, [[\label{species-2}]])
+    Append(expected, [[\paragraph{Species-3}]])
+    Append(expected, [[\label{species-3}]])
+    Append(expected, [[\subparagraph{Species-4}]])
+    Append(expected, [[\label{species-4}]])
+    return expected
+end
+
+for level = 1, 4 do
+    entitySetup(level)
+    local expected = generateExpected(level)
+    AssertAutomatedChapters("Entity with subentities and species-" .. level, expected, setup)
+end

f47a41bfabd7a80651ee1f29a1d77f7cc80a0166
TheComamba <thecomamba@jojoheinze.de>
2025-04-17 15:07:20 +0200

    Corrected script.

--- scripts/get_local_lorecore_build.sh
+++ scripts/get_local_lorecore_build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -e
-cd $(git rev-parse --show-toplevel)
+cd "$(git rev-parse --show-toplevel)"
 
 mkdir -p dependencies
 
 if [ "$(uname)" == "Darwin" ]; then
-    cp ../LoreCore/target/debug/liblorecore.dylib dependencies/
+    cp ../lore_core/target/debug/liblorecore.dylib dependencies/
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    cp ../LoreCore/target/debug/liblorecore.so dependencies/
+    cp ../lore_core/target/debug/liblorecore.so dependencies/
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW32_NT" ]; then
-    cp ../LoreCore/target/debug/lorecore.dll dependencies/
+    cp ../lore_core/target/debug/lorecore.dll dependencies/
 elif [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
-    cp ../LoreCore/target/debug/lorecore.dll dependencies/
+    cp ../lore_core/target/debug/lorecore.dll dependencies/
 fi
-cp ../LoreCore/lorecore_api.h dependencies/
+cp ../lore_core/lorecore_api.h dependencies/
